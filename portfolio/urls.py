54f93db5241e7eaf67bbbacf64a5f8d115b1d29e
Scott Ha <123viet123@gmail.com>
2020-04-16 15:55:52 -0700

    Created first HTML file nested in jobs/templates/jobs/nick.html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import jobs.views.nick
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('nick/', name="nick")
+    path('nick', jobs.views.nick, name="nick")
 ]
