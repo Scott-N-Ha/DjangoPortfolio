af77dc7cd521a1246ce1b2fbec6ecedf7298d28b
Scott Ha <123viet123@gmail.com>
2020-04-16 18:34:31 -0700

    Set up an additional route for /jobs/<int: job_id> to detail the job on a singular page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,7 +21,8 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name="home")
+    path('', jobs.views.home, name="home"),
+    path('jobs/<int:job_id>', jobs.views.detail, name="detail"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
